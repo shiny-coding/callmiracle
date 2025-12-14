f92bd3f5077f38e7a7b19caacb3553cbe60bfa63
Shine <shinymiracles@gmail.com>
2025-12-14 11:38:38 +0400

    Update Configuration and Dependencies for Improved Localization and Compatibility
    - Updated the Next.js configuration to specify the path for internationalization.
    - Upgraded dependencies in package.json, including next, next-auth, next-intl, and others for enhanced functionality and security.
    - Modified TypeScript configuration to support new JSX transform and improved path resolution.
    - Enhanced i18n setup to validate locale and load messages correctly, ensuring better localization support.

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,12 +2,14 @@ import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { locales } from './config';
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale
 
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!locale || !locales.includes(locale as any)) notFound();
 
   return {
-      messages: (await import(`../messages/${locale}.json`)).default,
+      locale,
+      messages: (await import(`./messages/${locale}.json`)).default,
   };
 });
